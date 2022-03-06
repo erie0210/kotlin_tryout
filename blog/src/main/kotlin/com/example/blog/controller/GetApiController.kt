748061c7bc8ecad7bbc2f82c4af71465c2ac2068
henryhckoo <henry.hc.koo@gmail.com>
2022-03-06 16:01:10 +0900

    feat: 쿠폰 서비스 연동

--- blog/src/main/kotlin/com/example/blog/controller/GetApiController.kt
+++ blog/src/main/kotlin/com/example/blog/controller/GetApiController.kt
@@ -1,15 +1,34 @@
 package com.example.blog.controller
 
+import com.example.blog.service.CouponService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/jy")
-class GetApiController {
+class GetApiController(
+    private val couponService: CouponService
+) {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     fun getHello(): String {
-        return "hello jaeyoung";
+        return "hello jaeyoung"
+    }
+
+    @GetMapping("/coupon")
+    fun getCoupon(
+        @RequestParam("coupon_name") couponName: String,
+        @RequestParam("user_id") userId: Long
+    ): ResponseEntity<String> {
+        val success = couponService.getCoupon(couponName, userId)
+
+        if (success) {
+            return ResponseEntity.ok("쿠폰이 발급되었습니다. couponName: $couponName, userId: $userId")
+        }
+
+        throw IllegalStateException("이미 발급이 끝난 쿠폰이에요")
     }
 }
