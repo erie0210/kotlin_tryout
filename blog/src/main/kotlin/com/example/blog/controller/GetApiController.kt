1aed25a7bba0da49b06a39343378c64a2384a9be
henryhckoo <henry.hc.koo@gmail.com>
2022-03-06 16:06:00 +0900

    refac: 컨트롤러 메세지 수정

--- blog/src/main/kotlin/com/example/blog/controller/GetApiController.kt
+++ blog/src/main/kotlin/com/example/blog/controller/GetApiController.kt
@@ -25,10 +25,10 @@ class GetApiController(
     ): ResponseEntity<String> {
         val success = couponService.getCoupon(couponName, userId)
 
-        if (success) {
-            return ResponseEntity.ok("쿠폰이 발급되었습니다. couponName: $couponName, userId: $userId")
+        if (!success) {
+            throw IllegalStateException("이미 발급이 끝난 쿠폰이거나, 쿠폰 발급 과정 중 오류가 발생했어요. couponName: $couponName, userId: $userId")
         }
 
-        throw IllegalStateException("이미 발급이 끝난 쿠폰이에요")
+        return ResponseEntity.ok("쿠폰이 발급되었습니다. couponName: $couponName, userId: $userId")
     }
 }
